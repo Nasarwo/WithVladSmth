2c31875923f154133eb65525813d04b8a20793d9
DaGGeR <diamantschwazar@gmail.com>
2024-07-12 20:54:28 +0300

    lesson2

--- server/main.js
+++ server/main.js
@@ -1,16 +1,37 @@
-import express from 'express';
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const PORT = 5050;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Bydlo' + ` PORT ${PORT}`);
-})
+//Middleware
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Bydlo" + ` PORT ${PORT}`);
+});
+
+app.post("/auth/login", (req, res) => {
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      name: req.body.name,
+    },
+    "secret123"
+  );
+
+  res.status(200).json({
+    success: true,
+    huy: token,
+  });
+});
 
 app.listen(PORT, (err) => {
-    if(err) {
-        return console.log(err);
-    }
-    console.log(`Server working on port ${PORT}`)
-})
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`Server working on port ${PORT}`);
+});
